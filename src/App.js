24e2912d090a439b002af555cb3a23b9f1069936
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-02-21 17:21:11 +0000

    page imports created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 //page component imports
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
