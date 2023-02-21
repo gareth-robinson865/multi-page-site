b9caa792ab2293fd1aa217eb0fdcc56675805725
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-02-21 17:09:04 +0000

    lorem ipsum added

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,8 @@
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+        <h1>Contact Page</h1>
+        <p>Voluptate ad aute amet non sunt occaecat laborum ut. Sint sit esse sunt laboris ullamco eu labore Lorem do commodo officia reprehenderit. Cupidatat veniam dolor aliquip esse. Commodo proident esse aute officia. Culpa pariatur consequat irure reprehenderit sint minim adipisicing ut. Lorem sint cillum ea deserunt magna minim magna laboris pariatur mollit ipsum commodo esse proident.</p>
+    </div>
   )
 }
