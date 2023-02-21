fb4cf22ad627f11f89d65fcd867710482bfd226a
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-02-21 17:09:40 +0000

    lorem ipsum added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,8 @@
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1>About Page</h1>
+      <p>Eu duis qui ea nisi. Ex elit velit amet aliquip officia aliqua. Excepteur Lorem pariatur sit irure et in cillum voluptate. Esse fugiat aliquip nisi laborum adipisicing. Esse labore aliquip reprehenderit dolore cillum adipisicing consectetur cupidatat Lorem reprehenderit velit dolor veniam. Sit voluptate sint duis quis cillum ea culpa nostrud. Irure sunt incididunt laboris in proident eiusmod.</p>
+    </div>
   )
 }
