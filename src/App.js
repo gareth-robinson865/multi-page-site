f746226716c8dd3d781ba3eed8d28d27c84d8936
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-02-27 14:37:08 +0000

    Link component created for Contact me

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           <h1>My Articles</h1>
           <Link to="/">Home</Link>
           <Link to="/About">About</Link>
+          <Link to="/Contact">Contact Me</Link>
         </nav>
         <Switch>
           <Route exact path="/">
