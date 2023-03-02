c6b7addf5f6ef8d8fce694d20d1382a7a5bb7e8f
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-02 14:14:30 +0000

    link to attribute was not correct, did not line up with the route which was causing an error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ export default function Home() {
         <div key={article.id} className="card">
           <h3>{article.title}</h3>
           <p>{article.author}</p>
-          <Link to={`/articles/${article.id}`}>Read More</Link>
+          <Link to={`/article/${article.id}`}>Read More</Link>
         </div>
       ))} 
     </div>
