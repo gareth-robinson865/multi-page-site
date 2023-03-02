c805b018ff11575c128309cfc74ce654714dfe12
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-02 12:37:57 +0000

    content deleted to make room for the articles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,7 @@ export default function Home() {
   
   return (
     <div>
-        <h2>Homepage</h2>
-        <p>Proident cillum commodo dolore aute laboris ex ea ex nulla exercitation. Magna eiusmod laboris consequat ea excepteur velit fugiat commodo nostrud enim ipsum non deserunt aliqua. Reprehenderit aliquip officia exercitation commodo officia velit quis. Incididunt et aliqua duis labore consectetur quis labore fugiat.</p>
+      
     </div>
   )
 }
