4a7c06b1dc056d888b6b8acdfa63d99b714701d8
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-03 14:32:35 +0000

    url created with the local host for the articles concatenated with the id that is taken from the useParams object

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -3,7 +3,8 @@ import { useFetch } from "../hooks/useFetch";
 
 export default function Article() {
   const { id } = useParams();
-
+  const url = 'http://localhost:3000/articles' + id
+  const { data, isPending, error} = useFetch(url)
   return (
     <div>
         Article page is here { id }
