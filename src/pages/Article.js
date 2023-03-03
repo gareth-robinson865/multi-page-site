ab7b2b4e6d0d0733c102c765e72dbc8966be1e7c
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-03 14:34:25 +0000

    data renamed to article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -4,7 +4,7 @@ import { useFetch } from "../hooks/useFetch";
 export default function Article() {
   const { id } = useParams();
   const url = 'http://localhost:3000/articles' + id
-  const { data, isPending, error} = useFetch(url)
+  const { data: article, isPending, error} = useFetch(url)
   return (
     <div>
         Article page is here { id }
