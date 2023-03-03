06219ced7daa856553b932334bf174025beb2feb
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-03 14:21:22 +0000

    destructured id parameter used to test if working

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom"
 
 export default function Article() {
+  const { id } = useParams();
+
   return (
     <div>
-        Article page is here
+        Article page is here { id }
     </div>
   )
 }
