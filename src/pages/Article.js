a2924a504a83d8d310603d06003d1b9c8bd40e99
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-04 12:26:38 +0000

    spacing changed for readability

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -2,9 +2,9 @@ import { useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch";
 
 export default function Article() {
-  const { id } = useParams();
+  const { id } = useParams()
   const url = 'http://localhost:3000/articles' + id
-  const { data: article, isPending, error} = useFetch(url)
+  const { data: article, isPending, error } = useFetch(url)
   return (
     <div>
         {isPending && <div>Loading...</div>}
