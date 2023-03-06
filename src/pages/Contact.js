7d54d5524a351ba767681e50fd03ac59830f9927
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-06 14:44:08 +0000

    error corrected, required key from url had to be in quotes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,9 +4,12 @@ export default function Contact() {
   const queryString = useLocation().search
   console.log(queryString)
 
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+
   return (
     <div>
-        <h1>Contact Page</h1>
+        <h1>Hi {name}, this is the Contact Page</h1>
         <p>Voluptate ad aute amet non sunt occaecat laborum ut. Sint sit esse sunt laboris ullamco eu labore Lorem do commodo officia reprehenderit. Cupidatat veniam dolor aliquip esse. Commodo proident esse aute officia. Culpa pariatur consequat irure reprehenderit sint minim adipisicing ut. Lorem sint cillum ea deserunt magna minim magna laboris pariatur mollit ipsum commodo esse proident.</p>
     </div>
   )
