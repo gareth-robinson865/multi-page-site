0b77dfe09c5e2e91db42f05fd1ec53457bca61f6
Gareth Robinson <gazz_1998@hotmail.co.uk>
2023-03-06 14:10:06 +0000

    comment removed

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch";
-//not working needs work
+
 export default function Article() {
   const { id } = useParams()
   const url = 'http://localhost:3000/articles/' + id
